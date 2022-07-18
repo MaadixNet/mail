80482b16bd3b6eea7f81c5cf4b5116b23a92e8e7
Anna Larch <anna@nextcloud.com>
2022-07-18 15:38:02 +0200

    fixup! Add imip processing

--- lib/Db/MessageMapper.php
+++ lib/Db/MessageMapper.php
@@ -282,7 +282,7 @@ class MessageMapper extends QBMapper {
 			$qb1->setValue('flag_notjunk', $qb1->createParameter('flag_notjunk'));
 			$qb1->setValue('flag_important', $qb1->createParameter('flag_important'));
 			$qb1->setValue('flag_mdnsent', $qb1->createParameter('flag_mdnsent'));
-//			$qb1->setValue('imip_message', $qb1->createParameter('imip_message'));
+			$qb1->setValue('imip_message', $qb1->createParameter('imip_message'));
 			$qb2 = $this->db->getQueryBuilder();
 
 			$qb2->insert('mail_recipients')
@@ -313,7 +313,7 @@ class MessageMapper extends QBMapper {
 				$qb1->setParameter('flag_notjunk', $message->getFlagNotjunk(), IQueryBuilder::PARAM_BOOL);
 				$qb1->setParameter('flag_important', $message->getFlagImportant(), IQueryBuilder::PARAM_BOOL);
 				$qb1->setParameter('flag_mdnsent', $message->getFlagMdnsent(), IQueryBuilder::PARAM_BOOL);
-//				$qb1->setParameter('imip_message', $message->isImipMessage(), IQueryBuilder::PARAM_BOOL);
+				$qb1->setParameter('imip_message', $message->isImipMessage(), IQueryBuilder::PARAM_BOOL);
 
 				$qb1->execute();
 
